4aabff1243a01dab33df352aee06fd72050fb60d
siiverfish <izimmerman@berkeley.edu>
2025-06-26 10:32:09 -0700

    graph changes, spin work

--- lib/lock/spin_lock.cpp
+++ lib/lock/spin_lock.cpp
@@ -1,5 +1,8 @@
 #include "lock.hpp"
-#include <stdatomic.h>
+#include <atomic>
+#include <stdio.h>
+#include <cstdlib>
+#include <time.h>
 
 class SpinLock : public virtual SoftwareMutex {
 public:
@@ -9,15 +12,22 @@ public:
 
     void lock(size_t thread_id) override {
         (void)thread_id; // This parameter is not used in this implementation
-        bool false_ = false;
-        while (!std::atomic_compare_exchange_weak(&locked, &false_, true));
+
+        while (true) {
+            if (std::atomic_flag_test_and_set_explicit(&lock_, std::memory_order_acquire)) {
+                break;
+            }
+            // nanosleep(&nanosleep_timespec, &remaining);
+        }
     }
     void unlock(size_t thread_id) override {
         (void)thread_id; // This parameter is not used in this implementation
-        locked = false;
+        std::atomic_flag_clear_explicit(&lock_, std::memory_order_release);
     }
     void destroy() override {}
     
 private:
-    volatile std::atomic<bool> locked;
+    volatile std::atomic_flag lock_ = ATOMIC_FLAG_INIT;
+    // const struct timespec nanosleep_timespec = { 1, 0 };
+    // struct timespec remaining;
 };
