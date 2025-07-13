51397cd3b8a6c73a2be000f273c34f5c74d38c10
Kush Goel <kush.goel@scriptsense.co.nz>
2025-07-13 12:03:19 -0700

    Switched to Buhr's corectness scheme

--- lib/lock/lock.hpp
+++ lib/lock/lock.hpp
@@ -5,7 +5,8 @@
 
 #include <atomic>
 #include <thread>
-#include <vector>
+#include <semaphore>
+#include "../utils/bench_utils.hpp"
 
 class SoftwareMutex {
 public:
@@ -24,9 +25,32 @@ public:
     // Cleanup any resources used by the mutex
     virtual void destroy() = 0;
 
-    std::condition_variable sleeper;
+    void criticalSection(size_t thread_id) {
+        *currentId=thread_id;
+        Fence();
+        for (int i=0; i<100; i++){
+            if (*currentId!= thread_id){
+                throw std::runtime_error(name() + " was breached");
+            }
+        }
+
+    }
+
+    void sleep() {
+        sleeper.acquire();
+    }
+
+    void wake(){
+        if(!sleeper.try_acquire()){
+        }
+            sleeper.release();
+    }
+
+    std::binary_semaphore sleeper{0};
 
     virtual std::string name() =0;
+private:
+    volatile unsigned int* currentId= (volatile unsigned int* )malloc(sizeof(unsigned int *));
 };
 
 #endif // LOCK_LOCK_HPP
