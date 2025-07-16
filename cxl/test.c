d52546bf2f6c71210aa67881823784d6137e2fdc
siiverfish <izimmerman@berkeley.edu>
2025-07-16 00:33:14 -0700

    update test.c

--- cxl/test.c
+++ cxl/test.c
@@ -11,8 +11,10 @@ void test_bakery()
     size_t region_size = bakery_static_mutex_get_size(NUM_THREADS);
     void *region = emucxl_alloc(region_size, REMOTE_CXL_MEMORY);
     struct bakery_static_mutex *mutex = bakery_static_mutex_init(region, NUM_THREADS);
-    bakery_static_mutex_lock(mutex, 0);
-    bakery_static_mutex_unlock(mutex, 0);
+    for (int i = 0; i < NUM_THREADS; i++) {
+        bakery_static_mutex_lock(mutex, i);
+        bakery_static_mutex_unlock(mutex, i);
+    }
     emucxl_free(region, region_size);
 }
 
@@ -20,9 +22,12 @@ int main(int argc, char **argv)
 {
     emucxl_init();
 
+    printf("Testing bakery... ");
     test_bakery();
+    printf("done\n");
 
     emucxl_exit();
 
+    printf("Done testing.\n");
     return 0;
 }
