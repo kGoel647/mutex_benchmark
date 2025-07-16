5003e7ebadd53edf8ee396c4b981b15c061e1def
siiverfish <izimmerman@berkeley.edu>
2025-07-16 11:07:07 -0700

    remove emucxl from bakery_static_mutex, multithread test

--- cxl/bakery_static_mutex.c
+++ cxl/bakery_static_mutex.c
@@ -1,5 +1,3 @@
-#include "emucxl/src/emucxl_lib.h"
-
 #include <stdint.h>
 #include <stddef.h>
 #include <stdbool.h>
@@ -18,9 +16,9 @@
 
 // These have to be statics so that we can get pointers to them
 // Should be constant, but the API doesn't allow it.
-static bool true_ = true;
-static bool false_ = false;
-static size_t not_in_contention = 0;
+// static bool true_ = true;
+// static bool false_ = false;
+// static size_t not_in_contention = 0;
 
 struct bakery_static_mutex {
     size_t num_threads;
@@ -53,25 +51,23 @@ struct bakery_static_mutex *bakery_static_mutex_init(void *region, size_t num_th
 void bakery_static_mutex_lock(struct bakery_static_mutex *mutex, size_t thread_id) 
 {
     // Get "bakery number"
-    size_t num_threads;
-    emucxl_read(&mutex->num_threads, 0, &num_threads, sizeof(mutex->num_threads));
+    size_t num_threads = mutex->num_threads;
     bool *choosing = bm_get_choosing_array(mutex, num_threads);
     size_t *number = bm_get_number_array(mutex, num_threads);
 
-    emucxl_write(choosing, thread_id, &true_, sizeof(bool));
+    choosing[thread_id] = true;
     atomic_thread_fence(memory_order_seq_cst);
     size_t my_bakery_number = 1;
     for (size_t i = 0; i < num_threads; i++) {
-        size_t other_number;
-        emucxl_read(number, i, &other_number, sizeof(size_t));
+        size_t other_number = number[i];
         if (other_number + 1 > my_bakery_number) {
             my_bakery_number = other_number + 1;
         }
     }
-    emucxl_write(number, thread_id, &my_bakery_number, sizeof(size_t));
+    number[thread_id] = my_bakery_number;
 
     atomic_thread_fence(memory_order_seq_cst);
-    emucxl_write(choosing, thread_id, &false_, sizeof(bool));
+    choosing[thread_id] = false;
     atomic_thread_fence(memory_order_seq_cst);
 
     // Lock waiting part
@@ -79,14 +75,14 @@ void bakery_static_mutex_lock(struct bakery_static_mutex *mutex, size_t thread_i
         // Wait until thread j is finished choosing its number if it's just now writing.
         bool other_thread_choosing;
         do {
-            emucxl_read(choosing, j, &other_thread_choosing, sizeof(bool));
+            other_thread_choosing = choosing[j];
         }
         while (other_thread_choosing != 0);
 
-        // Wait until we have the lower number between us and thread j.
-        size_t other_number, thread_id_number;
+        // Wait until our number is less than thread[j]'s.
+        size_t other_number;
         do {
-            emucxl_read(number, j, &other_number, sizeof(*number));
+            other_number = number[j];
         } while ((other_number != 0 && other_number < my_bakery_number) 
             || (other_number == my_bakery_number && j < thread_id));
     }
@@ -97,8 +93,6 @@ void bakery_static_mutex_lock(struct bakery_static_mutex *mutex, size_t thread_i
 void bakery_static_mutex_unlock(struct bakery_static_mutex *mutex, size_t thread_id)
 {
     atomic_thread_fence(memory_order_seq_cst);
-    size_t num_threads;
-    emucxl_read(&mutex->num_threads, 0, &num_threads, sizeof(mutex->num_threads));
-    size_t *number = bm_get_number_array(mutex, num_threads);
-    emucxl_read(number, thread_id, &not_in_contention, sizeof(*number));
+    size_t *number = bm_get_number_array(mutex, mutex->num_threads);
+    number[thread_id] = 0;
 }
--- cxl/test.c
+++ cxl/test.c
@@ -1,20 +1,61 @@
 #include "bakery_static_mutex.c"
 #include "emucxl/src/emucxl_lib.h"
 
+// #include <threads.h>
+#include <bits/pthreadtypes.h>
+#include <pthread.h>
+
 #define NUM_THREADS 10
+#define NUM_ITERATIONS 1000
 
 #define LOCAL_MEMORY 0
 #define REMOTE_CXL_MEMORY 1
+
+struct thread_info {
+    size_t thread_id;
+    struct bakery_static_mutex *mutex;
+    size_t *counter;
+    bool *start_flag;
+};
+
+static void *thread_start(void *arg)
+{
+    struct thread_info *thread_info = (struct thread_info*)arg;
+    while (!thread_info->start_flag);
+
+    for (size_t i = 0; i < NUM_ITERATIONS; i++) {
+        bakery_static_mutex_lock(thread_info->mutex, thread_info->thread_id);
+        thread_info->counter++;
+        bakery_static_mutex_unlock(thread_info->mutex, thread_info->thread_id);
+    }
+}
 
 void test_bakery()
 {
     size_t region_size = bakery_static_mutex_get_size(NUM_THREADS);
     void *region = emucxl_alloc(region_size, REMOTE_CXL_MEMORY);
     struct bakery_static_mutex *mutex = bakery_static_mutex_init(region, NUM_THREADS);
-    for (int i = 0; i < NUM_THREADS; i++) {
-        bakery_static_mutex_lock(mutex, i);
-        bakery_static_mutex_unlock(mutex, i);
+    // Should these be in shared memory?
+    size_t counter;
+    bool start_flag = false;
+    pthread_t threads[NUM_THREADS];
+    struct thread_info threads_info[NUM_THREADS];
+    
+    for (size_t thread_id = 0; thread_id < NUM_THREADS; thread_id++)
+    {
+        struct thread_info info = { thread_id, mutex, &counter, &start_flag };
+        threads_info[thread_id] = info;
+        pthread_create(&threads[thread_id], NULL, &thread_start, &threads_info[thread_id]);
     }
+
+    start_flag = true;
+
+    void *_ret;
+    for (size_t thread_id = 0; thread_id < NUM_THREADS; thread_id++)
+    {
+        pthread_join(threads[thread_id], &_ret);
+    }
+
     emucxl_free(region, region_size);
 }
 
