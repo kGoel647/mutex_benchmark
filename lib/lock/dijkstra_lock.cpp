ff0e8d0e156be6d906f4ecca22ed40352afb5bce
siiverfish <izimmerman@berkeley.edu>
2025-07-22 15:11:30 -0700

    CXl support for dijkstra

--- lib/lock/dijkstra_lock.cpp
+++ lib/lock/dijkstra_lock.cpp
@@ -1,16 +1,24 @@
 #include "lock.hpp"
+#include "../utils/cxl_utils.hpp"
 #include <stdexcept>
 
 class DijkstraMutex : public virtual SoftwareMutex {
 public:
     void init(size_t num_threads) override {
-        this->unlocking = (volatile std::atomic_bool*)malloc(sizeof(std::atomic_bool) * num_threads);
-        this->c         = (volatile std::atomic_bool*)malloc(sizeof(std::atomic_bool) * num_threads);
+        size_t k_size = sizeof(std::atomic_size_t);
+        size_t unlocking_size = sizeof(std::atomic_bool) * num_threads;
+        size_t c_size = sizeof(std::atomic_bool) * num_threads;
+        _cxl_region_size = sizeof(std::atomic_size_t) + unlocking_size + c_size;
+        _cxl_region = (volatile char*)ALLOCATE(_cxl_region_size);
+
+        this->k = (std::atomic_size_t*)&_cxl_region[0];
+        this->unlocking = (std::atomic_bool*)&_cxl_region[k_size];
+        this->c         = (std::atomic_bool*)&_cxl_region[k_size + unlocking_size];
         for (size_t i = 0; i < num_threads; i++) {
             unlocking[i] = true;
             c[i] = true;
         }
-        this->k = 0;
+        *this->k = 0;
         this->num_threads = num_threads;
     }
 
@@ -19,9 +27,9 @@ public:
         unlocking[thread_id] = false;
     try_again:
         c[thread_id] = true;
-        if (k != thread_id) {
-            while (!unlocking[k]) {}
-            k = thread_id;
+        if (*k != thread_id) {
+            while (!unlocking[*k]) {}
+            *k = thread_id;
             
             goto try_again;
         } 
@@ -40,15 +48,16 @@ public:
     }
 
     void destroy() override {
-        free((void*)unlocking);
-        free((void*)c);
+        FREE((void*)_cxl_region, _cxl_region_size);
     }
 
     std::string name(){return "djikstra";};
 
 private:
-    volatile std::atomic_bool *unlocking;
-    volatile std::atomic_bool *c;
-    volatile std::atomic<size_t> k;
+    volatile char *_cxl_region;
+    size_t _cxl_region_size; // this could just be re-calculated
+    std::atomic_bool *unlocking;
+    std::atomic_bool *c;
+    std::atomic_size_t *k;
     size_t num_threads;
 };
