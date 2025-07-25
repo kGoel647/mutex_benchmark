47d3bc99e61dc7dc5537cdcb390276005fc45c6d
Kush Goel <kush.goel@scriptsense.co.nz>
2025-07-25 11:55:33 -0700

    fix: dijkstra (now fair)

--- lib/lock/dijkstra_nonatomic_lock.cpp
+++ lib/lock/dijkstra_nonatomic_lock.cpp
@@ -7,41 +7,42 @@ public:
     void init(size_t num_threads) override {
         this->unlocking = (volatile bool*)malloc(sizeof(bool) * (num_threads+1));
         this->c = (volatile bool*)malloc(sizeof(bool) * (num_threads+1));
-        for (size_t i = 0; i < num_threads+1; i++) {
+        for (size_t i = 0; i <= num_threads; i++) {
             unlocking[i] = true;
             c[i] = true;
         }
-        this->k = num_threads;
+        this->k = 0;
         this->num_threads = num_threads;
     }
 
     void lock(size_t thread_id) override {
         // TODO refactor and remove goto
-        unlocking[thread_id] = false;
+        unlocking[thread_id+1] = false;
     try_again:
-        c[thread_id] = true;
+        c[thread_id+1] = true;
         Fence();
-        if (k != thread_id) {
+        if (k != thread_id+1) {
             while (!unlocking[k]) {}
-            k = thread_id;
+            k = thread_id+1;
 
             Fence();
             // goto try_again; //maybe not needed
 
         } 
-        c[thread_id] = false;
+        c[thread_id+1] = false;
         Fence();
-        for (size_t j = 0; j < num_threads; j++) {
-            if (j != thread_id && !c[j]) {
+        for (size_t j = 1; j <= num_threads; j++) {
+            if (j != thread_id +1 && !c[j]) {
                 goto try_again;
             }
         }
+        Fence();
 
     }
     void unlock(size_t thread_id) override {
-        k=num_threads;
-        unlocking[thread_id] = true;
-        c[thread_id] = true;
+        k=0;
+        unlocking[thread_id+1] = true;
+        c[thread_id+1] = true;
     }
     void destroy() override {
         free((void*)unlocking);
